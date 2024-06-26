afda620f2a8bfa0b7ccfd3ae059ad94c2eb438eb
stevenportkey <steven@portkey.finance>
2024-06-26 10:33:10 +0800

    Complete Poseidon hash

--- ProvingService/Services/IIdentifierHashService.cs
+++ ProvingService/Services/IIdentifierHashService.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using AElf;
+using Portkey.JwtProof.Extensions;
 using ProvingService.Helpers;
 
 namespace ProvingService.Services;
@@ -31,11 +33,70 @@ public class PoseidonIdentifierHashService : IIdentifierHashService
 {
     public string GenerateIdentifierHash(string subject, byte[] salt)
     {
-        return "Not implemented";
+        const int requiredSubjectLength = 255;
+
+        var subjectBytes = Encoding.ASCII.GetBytes(subject);
+
+        var hashString =
+            new Poseidon.Net.Poseidon().Hash(ChunksToFieldElements(subjectBytes, requiredSubjectLength, 31));
+
+        return new Poseidon.Net.Poseidon().Hash(new List<string>()
+            { hashString, ChunksToFieldElements(salt, 16, 16).First() });
+    }
+
+    private List<string> ChunksToFieldElements(byte[] bytes, int requiredLength, int chunkSize = 31)
+    {
+        var paddedBytes = PadSucceedingZeros(bytes, requiredLength);
+        var bigEndianSubjectBytes = paddedBytes.Reverse().ToArray();
+
+        var chunks = SplitByteArrayFromEnd(bigEndianSubjectBytes, chunkSize);
+        return chunks.Select(chunk => new BigInteger(chunk, true, true).ToString()).ToList();
+    }
+
+    private static byte[] PadSucceedingZeros(byte[] original, int length)
+    {
+        if (original.Length >= length)
+            return original;
+        var result = new byte[length];
+        Array.Copy(original, result, original.Length);
+        return result;
     }
 
     public List<string> ToPublicInput(string identifierHash)
     {
         return [identifierHash];
+    }
+
+    private static byte[][] SplitByteArrayFromEnd(byte[] buffer, int chunkSize)
+    {
+        var numChunks = (buffer.Length + chunkSize - 1) / chunkSize;
+        var chunks = new byte[numChunks][];
+
+        if (numChunks == 1)
+        {
+            chunks[0] = new byte[buffer.Length];
+            Array.Copy(buffer, 0, chunks[0], 0, buffer.Length);
+        }
+
+        var currentIndex = buffer.Length - chunkSize;
+        var currentChunkIndex = 0;
+        while (true)
+        {
+            var currentChunkSize = Math.Min(chunkSize, buffer.Length - currentChunkIndex * chunkSize);
+            chunks[currentChunkIndex] = new byte[currentChunkSize];
+            Array.Copy(buffer, currentIndex, chunks[currentChunkIndex], 0, currentChunkSize);
+            if (currentIndex == 0)
+            {
+                break;
+            }
+            else
+            {
+                currentIndex = Math.Max(0, currentIndex - currentChunkSize);
+            }
+
+            currentChunkIndex++;
+        }
+
+        return chunks;
     }
 }
